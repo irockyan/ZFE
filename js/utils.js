5130e55378fc7aaeacaf669f381b70abd3e22d0b
Zyan <coderZyan@outlook.com>
2017-03-09 11:56:03 +0800

    utils:增加随机数工具

--- js/utils.js
+++ js/utils.js
@@ -2,7 +2,7 @@
  * 获取格式话的时间
  * @param {*时间戳} timeStamp 
  */
-const getFormateDate = (timeStamp) => {
+export const getFormateDate = (timeStamp) => {
     const date = new Date(timeStamp);
     let year = date.getFullYear(),
         month = date.getMonth() < 10 ? '0' + (date.getMonth() + 1)  : date.getMonth(),
@@ -10,4 +10,14 @@ const getFormateDate = (timeStamp) => {
     return year + '-' + month + '-' + day;
 }
 
+/**
+ * 获得随机数
+ * @param {*起始数字} startNum 
+ * @param {*结尾数字} endNum 
+ */
+
+export const getRandom = (startNum, endNum) => {
+        var iChoice = endNum - startNum;
+        return Math.floor(Math.random() * iChoice + startNum);
+}
 console.log(getFormateDate(Date.now()));
